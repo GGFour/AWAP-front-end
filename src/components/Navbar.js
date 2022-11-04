49fbc9de8c1f2f4dfb660ce96133668b7af14d1e
Surtaev <vladimir.surtaev@nokia.com>
2022-11-04 12:16:31 +0200

    Add formating scripts
    Add npm scripts and format code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react'
 
 function Navbar() {
-  return (
-    <div className="navbar">
-      <div className=" navbar-left">
-        <a href="">visualization</a>
-      </div>
-      <div className="hidden"></div>
-      <div></div>
-      <div className="navbar-right">
-        <a href="">Login</a>
-        <a href="">sign up</a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar">
+            <div className=" navbar-left">
+                <a href="">visualization</a>
+            </div>
+            <div className="hidden"></div>
+            <div></div>
+            <div className="navbar-right">
+                <a href="">Login</a>
+                <a href="">sign up</a>
+            </div>
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,22 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React from 'react'
+import Navbar from '../components/Navbar'
 
 function Home() {
-  return (
-    <div className="container">
-      <Navbar></Navbar>
-      <div className="body">
-        <div className="body-text">
-          <h1>data center</h1>
-          <p>
-            explore different visualization of <br />
-            data represented of our climate
-          </p>
+    return (
+        <div className="container">
+            <Navbar></Navbar>
+            <div className="body">
+                <div className="body-text">
+                    <h1>data center</h1>
+                    <p>
+                        explore different visualization of <br />
+                        data represented of our climate
+                    </p>
+                </div>
+                <div className="visualization-views"></div>
+            </div>
         </div>
-        <div className="visualization-views"></div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Home;
+export default Home
