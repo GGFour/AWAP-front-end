cefd76f73e190ac5e650a9cd11ce6dfd44940cb2
hajri <c1almo00@students.oamk.fi>
2022-11-10 13:12:21 +0200

    signup & login components, form validation and popup css design added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Signup from './Signup'
+import Login from './Login'
 
 function Navbar() {
+    const [signupStatus, setSignupStatus] = useState(false)
+    const [loginStatus, setLoginStatus] = useState(false)
+
+    const openSignup = () => {
+        setSignupStatus(!signupStatus)
+    }
+    const openLogin = () => {
+        setLoginStatus(!loginStatus)
+    }
+    const closeModal = () => {
+        setSignupStatus(false)
+        setLoginStatus(false)
+    }
     return (
         <div className="navbar">
             <div className=" navbar-left">
@@ -9,11 +24,27 @@ function Navbar() {
             <div className="hidden"></div>
             <div></div>
             <div className="navbar-right">
-                <a href="">Login</a>
-                <a href="">sign up</a>
+                <a href="#" onClick={openLogin}>
+                    Login
+                </a>
+                <a href="#" onClick={openSignup}>
+                    signup
+                </a>
+                {signupStatus || loginStatus ? (
+                    <div className="main">
+                        <div className="popup">
+                            <div onClick={closeModal} className="closing-btn">
+                                x
+                            </div>
+                            {signupStatus ? <Signup /> : ''}
+                            {loginStatus ? <Login /> : ''}
+                        </div>
+                    </div>
+                ) : (
+                    ''
+                )}
             </div>
         </div>
     )
 }
-
 export default Navbar
