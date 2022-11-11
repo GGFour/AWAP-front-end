866776bde3e0195b2ca477ae65a540018fa2f59b
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-11-11 11:06:44 +0200

    Formatting fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,93 +2,90 @@ import React from 'react'
 import { useState } from 'react'
 
 function Form(props) {
-    const [focused, setFocused] = useState(false)
-    const [values, setValues] = useState({
-        username: '',
-        password: '',
-        confirmPassword: '',
-    })
+  const [focused, setFocused] = useState(false)
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
 
-    const inputs = [
-        {
-            id: 1,
-            name: 'username',
-            type: 'text',
-            placeholder: 'Type a Username',
-            label: 'Username',
-            pattern: '^[A-Za-z0-9]{3,15}$',
-            errorMessage:
-                'Username must have 3-15 characters with no special characters',
-            required: true,
-            loginInput: true,
-        },
-        {
-            id: 2,
-            name: 'password',
-            type: 'password',
-            placeholder: 'Enter a password',
-            label: 'Password',
-            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-            errorMessage:
-                'Password should be at least 8 characters long containing at least 1 letter, 1 number and 1 special character!',
-            required: true,
-            loginInput: true,
-        },
-        {
-            id: 3,
-            name: 'confirmPassword',
-            type: 'password',
-            placeholder: 'Confirm pasword',
-            label: 'Confirm Password',
-            pattern: values.password,
-            errorMessage: 'passwords do not match!',
-            required: true,
-            loginInput: false,
-        },
-    ]
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Type a Username',
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,15}$',
+      errorMessage:
+        'Username must have 3-15 characters with no special characters',
+      required: true,
+      loginInput: true,
+    },
+    {
+      id: 2,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Enter a password',
+      label: 'Password',
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      errorMessage:
+        'Password should be at least 8 characters long containing at least 1 letter, 1 number and 1 special character!',
+      required: true,
+      loginInput: true,
+    },
+    {
+      id: 3,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm pasword',
+      label: 'Confirm Password',
+      pattern: values.password,
+      errorMessage: 'passwords do not match!',
+      required: true,
+      loginInput: false,
+    },
+  ]
 
-    const onChange = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value })
-    }
-    const handleFocus = (e) => {
-        setFocused(true)
-    }
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+  const handleFocus = (e) => {
+    setFocused(true)
+  }
 
-    return (
-        <>
-            <div className="form-container">
-                <form onSubmit={props.handleSubmit}>
-                    <div>
-                        {inputs.map((input) => {
-                            return input.loginInput || props.signUp ? (
-                                <>
-                                    <label>{input.label}</label>
-                                    <input
-                                        key={input.id}
-                                        {...input}
-                                        values={values[input.name]}
-                                        onChange={onChange}
-                                        onBlur={handleFocus}
-                                        onFocus={() =>
-                                            input.name === 'confirmPassword' &&
-                                            setFocused(true)
-                                        }
-                                        focused={focused.toString()}
-                                    />
+  return (
+    <>
+      <div className="form-container">
+        <form onSubmit={props.handleSubmit}>
+          <div>
+            {inputs.map((input) => {
+              return input.loginInput || props.signUp ? (
+                <>
+                  <label>{input.label}</label>
+                  <input
+                    key={input.id}
+                    {...input}
+                    values={values[input.name]}
+                    onChange={onChange}
+                    onBlur={handleFocus}
+                    onFocus={() =>
+                      input.name === 'confirmPassword' && setFocused(true)
+                    }
+                    focused={focused.toString()}
+                  />
 
-                                    <span className="errorMes">
-                                        {input.errorMessage}
-                                    </span>
-                                </>
-                            ) : (
-                                <></>
-                            )
-                        })}
-                    </div>
-                </form>
-            </div>
-        </>
-    )
+                  <span className="errorMes">{input.errorMessage}</span>
+                </>
+              ) : (
+                <></>
+              )
+            })}
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default Form
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,23 +2,23 @@ import React from 'react'
 import Form from './Form'
 
 function Signup() {
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
-    return (
-        <>
-            <div className="signup-header">
-                <h1>Signup</h1>
-            </div>
-            <Form signUp={true} handleSubmit={handleSubmit}></Form>
-            <div className="button-div">
-                <button>Submit</button>
-                <p>
-                    Already have an account? <a href="#">Login</a>
-                </p>
-            </div>
-        </>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      <div className="signup-header">
+        <h1>Signup</h1>
+      </div>
+      <Form signUp={true} handleSubmit={handleSubmit}></Form>
+      <div className="button-div">
+        <button>Submit</button>
+        <p>
+          Already have an account? <a href="#">Login</a>
+        </p>
+      </div>
+    </>
+  )
 }
 
 export default Signup
