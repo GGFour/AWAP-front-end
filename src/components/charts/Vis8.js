add1e3b741ed52c6451919eff152dc74e78ba8cf
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-11-28 22:18:53 +0200

    Attach vis8 to back-end

--- src/components/charts/Vis8.js
+++ src/components/charts/Vis8.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Scatter } from 'react-chartjs-2'
-import DATA from '../../data/vis8/vis8.json'
-//import zoomPlugin from 'chartjs-plugin-zoom'
+import axios from 'axios'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -21,50 +20,66 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-  //zoomPlugin
 )
 
-export const options = {
-  responsive: true,
-  parsing: false,
-  plugins: {
-    legend: {
-      position: 'top',
+function getOptions(title, scales) {
+  const options = {
+    responsive: true,
+    showLine: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: title,
+      },
+    },
+    scales: scales,
+  }
+
+  return options
+}
+
+const scales = {
+  x: {
+    locale: 'en-US',
+    type: 'linear',
+    ticks: {
+      callback: function (value) {
+        return String(parseInt(value))
+      },
     },
     title: {
       display: true,
-      text: 'CO2 emissions by country',
+      text: 'Year',
     },
-    zoom: {
-      zoom: {
-        wheel: {
-          enabled: true,
-        },
-        pinch: {
-          enabled: true,
-        },
-        mode: 'x',
-      },
-    },
-    pan: {
-      enabled: true,
-      mode: 'x',
+  },
+  y: {
+    type: 'linear',
+    title: {
+      display: true,
+      text: 'CO2 Mt',
     },
   },
 }
 
+const URL = 'http://localhost:3000/api/visualization8'
+const STD = { data: [] }
+
 function Vis8() {
-  const [data, setData] = useState([])
-  const [countries, setCountries] = useState([])
+  const [data, setData] = useState(STD)
   const [datasets, setDatasets] = useState([])
 
   useEffect(() => {
-    setCountries(
-      DATA.map((e) => e.country).filter((value, index, self) => {
-        return self.indexOf(value) === index
+    axios
+      .get(URL)
+      .then((response) => {
+        setData(response.data)
       })
-    )
-    setData(DATA)
+      .catch((err) => {
+        alert(err)
+      })
   }, [])
 
   useEffect(() => {
@@ -72,15 +87,15 @@ function Vis8() {
   }, [data])
 
   function generateDatasets() {
+    const countries = data.data
+      .map((e) => e.country)
+      .filter((value, index, self) => {
+        return self.indexOf(value) === index
+      })
     const datasets = countries.map((country, idx) => {
       return {
         label: country,
-        data: data
-          .filter((item) => item.country == country)
-          .map((item) => ({
-            x: Number.parseInt(item.year),
-            y: item['CO2_Mt'],
-          })),
+        data: data.data.filter((item) => item.country == country),
         showLine: true,
         borderColor: `hsl(${idx * (256 / countries.length)}, 50%, 50%)`,
         backgroundColor: 'rgba(256, 256, 256, 0.75)',
@@ -93,7 +108,6 @@ function Vis8() {
         },
       }
     })
-    // alert('datasets generated ' + data.length + ' ' + countries.length)
     return datasets
   }
 
@@ -101,9 +115,11 @@ function Vis8() {
     <>
       <div>Vis6</div>
       <div>Data length: {data.length}</div>
-      <div>countries length: {countries.length}</div>
       <div>Datasets length: {datasets.length}</div>
-      <Scatter data={{ datasets: datasets }} options={options} />
+      <Scatter
+        data={{ datasets: datasets }}
+        options={getOptions(data.name, scales)}
+      />
     </>
   )
 }
